597d1a7201a17f9dddd2705a9e9f1ae195fe2808
Nitin Misra <nitin.misra84@gmail.com>
2021-03-29 23:10:25 -0700

    4 tests

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../../App.js";
+
+// write a test that will check to see if the App title is 'School Picker'
---
+++ src/components/__tests__/Rater.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Rater } from "../Rater";
+
+describe("<Rater />", () => {
+  const testSchools = [
+    { name: "School 1" },
+    { name: "School 2" },
+    { name: "School 3" },
+  ];
+
+  // write a test that will check to see if the first school displays on initial load
+
+  // write a test that will check to see if the 2nd school displays when the next button is pressed
+
+  // write a test that will check to see if the 1st school displays when the previous button is pressed
+});

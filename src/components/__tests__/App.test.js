e4a0c9bb79d4820acd3be6691678def6e7fc9cee
Carlos Chavez <hamandatorres@gmail.com>
2021-03-30 11:02:37 -0600

    finished

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,3 +3,10 @@ import { render } from "@testing-library/react";
 import App from "../../App.js";
 
 // write a test that will check to see if the App title is 'School Picker'
+
+
+test('renders page title', () => {
+  const { getByText } = render(<App />);
+  const title = getByText('School Picker');
+  expect(title).toBeInTheDocument();
+});
--- src/components/__tests__/Rater.test.js
+++ src/components/__tests__/Rater.test.js
@@ -11,7 +11,35 @@ describe("<Rater />", () => {
 
   // write a test that will check to see if the first school displays on initial load
 
+  it('check to see if first school displays on initial load', () => {
+    const { getByText } = render(<Rater schools={testSchools} />)
+    const schoolElement = getByText(new RegExp(testSchools[0].name));
+    expect(schoolElement).toBeInTheDocument();
+  });
+
   // write a test that will check to see if the 2nd school displays when the next button is pressed
+
+  it('check to see if the 2nd school displays when next btn is pressed', () => {
+    const { getByText } = render(<Rater schools={testSchools} />)
+    const nextbutton = getByText(/Next/i)
+    nextbutton.click()
+    const schoolElement = getByText(new RegExp(testSchools[1].name));
+    expect(schoolElement).toBeInTheDocument();
+  })
 
   // write a test that will check to see if the 1st school displays when the previous button is pressed
 });
+
+it('check to see if previous school displays when btn is pressed', () => {
+  const realUseSTate = React.useState
+  const schoolIndex = 1
+
+  jest.spyOn(React, "useState").mockImplementationOnce(() => {
+    return realUseSTate(schoolIndex)
+  })
+  const { getByText } = render(<Rater schools={testSchools} />)
+  const prevButton = getByText(/Previous/i);
+  prevButton.click();
+  const schoolElement = getByText(new RegExp(testSchools[0].name));
+  expect(schoolElement).toBeInTheDocument();
+})
